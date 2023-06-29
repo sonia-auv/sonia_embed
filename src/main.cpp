6da616107b78b49dccca05abf9715de3b9d43ac3
supertoto29 <francisalonzo29@gmail.com>
2023-06-29 11:52:00 -0400

    addition of can bus test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define INTERFACE
+//#define INTERFACE
 #include "../include/main.h"
 
 // Blinking rate in milliseconds
@@ -11,26 +11,33 @@ int main(int argc, char const *argv[])
     char buff[2];
     char buff2[2];
 
+#ifdef SERIAL_TEST
     rs485_receiver_en = 0;
     rs485_driver_en = 1;
     rs485_term_en = 1;
     while (true) {
-//        buff[0]++;
-//        debug_link.write(buff,1);
-//        rs485.write(buff,1);
-//        ledYlw = !ledYlw;
-//        ThisThread::sleep_for(BLINKING_RATE);
-
-
         ftdi.read(buff,1);
         rs485.write(buff,1);
         ledRed = !ledRed;
         rs485.read(buff2,1);
         ftdi.write(buff2,1);
     }
+#else
+    CANMessage msg;
+    while (true) {
+        ftdi.read(buff,1);
+        can.write(CANMessage(123,buff,1));
+        ledRed = !ledRed;
+        while(can.read(msg) == 0) {}
+        ftdi.write(&(msg.data[0]),1);
+    }
+#endif
+
+
     #else
     char buff[2];
 
+#ifdef SERIAL_TEST
     rs485_receiver_en = 0;
     rs485_driver_en = 1;
     rs485_term_en = 1;
@@ -39,6 +46,14 @@ int main(int argc, char const *argv[])
         ledRed = !ledRed;
         rs485.write(buff,1);
     }
+#else
+    CANMessage msg;
+    while (true) {
+        while(can.read(msg) == 0) {};
+        ledRed = !ledRed;
+        can.write(CANMessage(124,&(msg.data[0]),1));
+    }
+#endif
     #endif
 
 }
