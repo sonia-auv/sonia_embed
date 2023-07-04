98b45e8627c123d218c09355a2e5e2e11caa5632
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2023-07-04 17:38:13 -0400

    fixed lib build

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 
+#ifdef SONIA_EMBED
 int main(int argc, char const *argv[])
 {
     return 0;
 }
+#endif
---
+++ src/serial_control.cpp
@@ -0,0 +1,28 @@
+#include "serial_control.h"
+
+namespace sonia_embed
+{
+    RETURN_CODE SerialControl::add_filter(size_t id)
+    {
+        if (m_table_index == MAX_IDS)
+        {
+            return RETURN_MAX_IDS_REACHED;
+        }
+        m_id_table[m_table_index] = id;
+        m_table_index++;
+        return RETURN_OK;
+    }
+
+    bool SerialControl::check_filter(size_t id)
+    {
+        for (size_t i = 0; i < m_table_index; i++)
+        {
+            if (id == m_id_table[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+} // namespace sonia_embed
