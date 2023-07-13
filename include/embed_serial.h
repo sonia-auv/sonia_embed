9196d0238284823517b68b2a451f584962b5b4c7
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2023-07-13 17:50:49 -0400

    file cleanup

--- include/embed_serial.h
+++ include/embed_serial.h
@@ -17,28 +17,4 @@ namespace sonia_embed
         RawSerial *m_serial_handler;
     };
 
-#pragma once
-
-#include "mbed.h"
-#include "serial_control.h"
-#include "return_codes.h"
-
-    namespace sonia_embed
-    {
-        class RS485Control : public SerialControl
-        {
-        public:
-            RS485Control(PinName hoci, PinName hico, int baud,
-                         PinName receiver_enable, PinName transmiter_enable,
-                         PinName termination_enable, bool is_host = false);
-            ~RS485Control();
-
-            std::pair<size_t, size_t> receive(uint8_t *data) override;
-            RETURN_CODE transmit(const size_t id, const uint8_t *data, const size_t size) override;
-
-        private:
-            RawSerial *m_serial_handler;
-            DigitalOut m_transmit_enable;
-        };
-    }
 }
